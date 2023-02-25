07a520a038c7d9af699cc61de4e26dc38e0ecb32
lefmanos <lefmanos@protonmail.com>
2023-02-25 16:16:11 +0200

    remove conky

--- dwm.c
+++ dwm.c
@@ -1780,7 +1780,6 @@ void
 runAutostart(void) {
 	// TODO maybe fix the line below for dwmblocks startup-crash
 	// system("killall -q dwmblocks; dwmblocks &");
-	system("/usr/bin/conky &");
 	system("/usr/bin/wmname LG3D || notify-send 'JavaRuntime' 'wmname app not installed'");
 }
 
