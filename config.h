0b4f5a0a65dbc7591a88cf16f1f5530167f28aee
lefmanos <lefmanos@protonmail.com>
2023-02-27 17:19:25 +0200

    remove systray

--- config.h
+++ config.h
@@ -18,11 +18,6 @@ static unsigned int gappoh    = 10;       /* horiz outer gap between windows and
 static unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
 static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 0;     /* 0 means no systray */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 //static char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
@@ -68,33 +63,33 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	*/
-	/* class				instance					title				tags mask		isfloating   isterminal  noswallow  monitor */
-	{ "Gimp",				NULL,						NULL,       	    1 << 8,					0,           0,         0,        -1},
-	{ TERMCLASS, 			NULL,						NULL,       	    0,            			0,           1,         0,        -1},
-	{ NULL,      			NULL,       				"Event Tester",		0,            			0,           0,         1,        -1},
-	{ NULL,		 			"ncmpcpp",	  				NULL,       	    0,            			1,           1,         0,        -1},
-	{ NULL,		 			"megasync",	  				NULL,       	    0,            			1,           0,         0,        -1},
-	{ NULL,		 			"cairo-dock",	  			NULL,       	    0x0ff,         			1,           0,         0,        -1},
-	{ NULL,		 			"qjackctl",  				NULL,       	    1 << 7,       			0,           0,         0,        -1},
-	{ NULL, 	 			NULL,						"EVALUATION",		1 << 5,       			1,           0,         0,        -1},
-	// { NULL, 	 			NULL,  	  					"abd",				1 << 2,       			0,           1,         0,        -1},
-	{ "Steam",	 			NULL,						NULL,				1 << 4,       			0,           0,         0,        -1},
-	{ "VirtualBox Manager",	NULL,		  				NULL,				1 << 4,       			0,           0,         0,        -1},
-	{ "trello-nativefier-a9fb22",NULL,		  				NULL,				1 << 4,       			0,           0,         0,        -1},
-	{ "Zathura",			NULL,		  				NULL,				1 << 1,       			0,           0,         0,        -1},
-	{ "GitAhead",			NULL,		  				NULL,				1 << 3,       			0,           0,         0,        -1},
-	{ "Slack",	 			NULL,		  				NULL,  				1 << 8,       			0,           0,         0,        -1},
-	{ "discord", 			NULL,						NULL,  				1 << 7,       			0,           0,         0,        -1},
-	{ "zoom",	 			NULL,						NULL,  				1 << 8,       			0,           0,         0,        -1},
-	{ NULL,		 			"jitsi meet",				NULL,				1 << 8,       			1,           1,         0,        -1},
-	{ NULL,	     			NULL,						"pulsemixer",	    0,            			1,           1,         0,        -1},
-	{ NULL,	     			"brave-browser",			NULL,				1,            			0,           1,         0,        -1},
-	{ NULL,      			"spterm",    				NULL,       	    SPTAG(0),     			1,           1,         0,        -1},
-	{ NULL,      			"spsignal",  				NULL,       	    1 << 8 | SPTAG(1),		1,           1,         0,        -1},
-	{ NULL,      			"sptele",    				NULL,       	    1 << 8 | SPTAG(2),     	1,           1,         0,        -1},
-	{ NULL,      			"spviber",   				NULL,       	    1 << 8 | SPTAG(3),     	1,           1,         0,        -1},
-	{ NULL,      			"spmess",   				NULL,       	    1 << 8 | SPTAG(5),     	1,           1,         0,        -1},
-	{ NULL,      			"spcalc",    				NULL,       	    SPTAG(4),				1,           1,         0,        -1},
+	/* class				instance					title				tags mask		isfloating   issticky   isterminal  noswallow  monitor */
+	{ "Gimp",				NULL,						NULL,       	    1 << 8,					0,           0,         0,         0,        -1},
+	{ TERMCLASS, 			NULL,						NULL,       	    0,            			0,           1,         1,         0,        -1},
+	{ NULL,      			NULL,       				"Event Tester",		0,            			0,           0,         0,         1,        -1},
+	{ NULL,		 			"ncmpcpp",	  				NULL,       	    0,            			1,           1,         1,         0,        -1},
+	{ NULL,		 			"megasync",	  				NULL,       	    0,            			1,           0,         0,         0,        -1},
+	{ NULL,		 			"cairo-dock",	  			NULL,       	    0,         			    1,           1,         0,         0,        -1},
+	{ NULL,		 			"qjackctl",  				NULL,       	    1 << 7,       			0,           0,         0,         0,        -1},
+	{ NULL, 	 			NULL,						"EVALUATION",		1 << 5,       			1,           0,         0,         0,        -1},
+	// { NULL, 	 			NULL,  	  					"abd",				1 << 2,       			0,           1,         1,         0,        -1},
+	{ "Steam",	 			NULL,						NULL,				1 << 4,       			0,           0,         0,         0,        -1},
+	{ "VirtualBox Manager",	NULL,		  				NULL,				1 << 4,       			0,           0,         0,         0,        -1},
+	{ "trello-nativefier-a9fb22",NULL,		  				NULL,				1 << 4,    			0,           0,         0,         0,        -1},
+	{ "Zathura",			NULL,		  				NULL,				1 << 1,       			0,           0,         0,         0,        -1},
+	{ "GitAhead",			NULL,		  				NULL,				1 << 3,       			0,           0,         0,         0,        -1},
+	{ "Slack",	 			NULL,		  				NULL,  				1 << 8,       			0,           0,         0,         0,        -1},
+	{ "discord", 			NULL,						NULL,  				1 << 7,       			0,           0,         0,         0,        -1},
+	{ "zoom",	 			NULL,						NULL,  				1 << 8,       			0,           0,         0,         0,        -1},
+	{ NULL,		 			"jitsi meet",				NULL,				1 << 8,       			1,           1,         1,         0,        -1},
+	{ NULL,	     			NULL,						"pulsemixer",	    0,            			1,           1,         1,         0,        -1},
+	{ NULL,	     			"brave-browser",			NULL,				1,            			0,           1,         1,         0,        -1},
+	{ NULL,      			"spterm",    				NULL,       	    SPTAG(0),     			1,           1,         1,         0,        -1},
+	{ NULL,      			"spsignal",  				NULL,       	    1 << 8 | SPTAG(1),		1,           1,         1,         0,        -1},
+	{ NULL,      			"sptele",    				NULL,       	    1 << 8 | SPTAG(2),     	1,           1,         1,         0,        -1},
+	{ NULL,      			"spviber",   				NULL,       	    1 << 8 | SPTAG(3),     	1,           1,         1,         0,        -1},
+	{ NULL,      			"spmess",   				NULL,       	    1 << 8 | SPTAG(5),     	1,           1,         1,         0,        -1},
+	{ NULL,      			"spcalc",    				NULL,       	    SPTAG(4),				1,           1,         1,         0,        -1},
 };
 
 /* layout(s) */
